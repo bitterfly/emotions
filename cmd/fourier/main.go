63bf414e65df57dfbdeb99d0224b7400f1c62727
bitterfly <dageneva@gmail.com>
2018-11-03 16:06:51 +0200

    fix merge conflict

--- cmd/fourier/main.go
+++ cmd/fourier/main.go
@@ -1,62 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"os"
 
 	"github.com/bitterfly/emotions/fourier"
 )
 
 func main() {
-	n := 2048
-	x := make([]float64, n, n)
-	y := make([]float64, n, n)
-	z := make([]float64, n, n)
-	c := make([]fourier.Complex, n, n)
-
-	for i := 0; i < n; i++ {
-		x[i] = 4 * math.Cos(math.Pi*float64(2*i*1000)/float64(n))
-		y[i] = 3 * math.Cos(math.Pi*float64(2*i*800)/float64(n))
-		z[i] = x[i] + y[i]
-		c[i] = fourier.Complex{Re: 4 * math.Cos(math.Pi*float64(2*i*1000)/float64(n)), Im: 0.0}
+	filename := os.Args[1]
+	wf, err := fourier.Read(filename)
+	if err != nil {
+		panic(err.Error)
 	}
-
-	xc, yc := fourier.DoubleReal(x, y)
-
-	fmt.Printf("X:\n")
-	for i, c := range xc {
-		if fourier.Magnitude(c) > fourier.EPS {
-			fmt.Printf("%d %s\n", i, c)
-		}
-	}
-
-	fmt.Printf("Y:\n")
-	for i, c := range yc {
-		if fourier.Magnitude(c) > fourier.EPS {
-			fmt.Printf("%d %s\n", i, c)
-		}
-	}
-
-	zc := fourier.FftReal(z)
-	fmt.Printf("Z real:\n")
-	for i, c := range zc {
-		if fourier.Magnitude(c) > fourier.EPS {
-			fmt.Printf("%d %s\n", i, c)
-		}
-	}
-
-	cc := fourier.Fft(c)
-	ccc := fourier.Ifft(cc)
-	fmt.Printf("C complex:\n")
-	for i, b := range ccc {
-		if fourier.Magnitude(b) > fourier.EPS {
-			fmt.Printf("%d %s %s\n", i, b, c[i])
-		}
-	}
-	// wf, err := fourier.Read(os.Args[1])
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	// signal := wf.GetData()
 
