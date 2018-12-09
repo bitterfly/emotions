5921d4ac268d53d0357bc2c826e40905923dafa6
bitterfly <dageneva@gmail.com>
2018-12-09 18:13:54 +0200

    Add file with different windows

--- fourier/windows.go
+++ fourier/windows.go
@@ -1,40 +1,40 @@
 package fourier
 
-import "math"
+// import "math"
 
-const WINDOW int = 1024
-const INNER_WINDOW = 768
+// const WINDOW int = 1024
+// const INNER_WINDOW = 768
 
-func f(x float64) float64 {
-	return 1 / (1 + math.Exp(-x))
-}
+// func f(x float64) float64 {
+// 	return 1 / (1 + math.Exp(-x))
+// }
 
-func rectangular(x float64) float64 {
-	return 1
-}
+// func rectangular(x float64) float64 {
+// 	return 1
+// }
 
-func hanning(x float64) float64 {
-	return 0.5 - 0.5*math.Cos(2*math.Pi*x/INNER_WINDOW)
-}
+// func hanning(x float64) float64 {
+// 	return 0.5 - 0.5*math.Cos(2*math.Pi*x/INNER_WINDOW)
+// }
 
-func hamming(x float64) float64 {
-	return 0.54 - 0.46*math.Cos(2*math.Pi*x/INNER_WINDOW)
-}
+// func hamming(x float64) float64 {
+// 	return 0.54 - 0.46*math.Cos(2*math.Pi*x/INNER_WINDOW)
+// }
 
-func window(content []float64, windowFunction func(float64) float64) {
-	for i, x := range content {
-		content[i] = x * (windowFunction(float64(i)-float64(uttBeg)) * windowFunction(float64(uttEnd)-float64(i)))
-	}
-}
+// func window(content []float64, windowFunction func(float64) float64) {
+// 	for i, x := range content {
+// 		content[i] = x * windowFunction(i)
+// 	}
+// }
 
-func hammingWindow(content []float64, uttBeg, uttEnd uint64) {
-	window(content, uttBeg, uttEnd, hamming)
-}
+// func hammingWindow(content []float64) {
+// 	window(content, hamming)
+// }
 
-func hanningWindow(content []float64, uttBeg, uttEnd uint64) {
-	window(content, uttBeg, uttEnd, hanning)
-}
+// func hanningWindow(content []float64) {
+// 	window(content, hanning)
+// }
 
-func rectangularWindow(content []float64, uttBeg, uttEnd uint64) {
-	window(content, uttBeg, uttEnd, rectangular)
-}
+// func rectangularWindow(content []float64) {
+// 	window(content, rectangular)
+// }
