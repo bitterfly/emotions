8980d5a308298c0ee7ebd59633683815d8dfefef
dageneva <dageneva@gmail.com>
2018-12-18 14:58:54 +0200

    Remove normalisation when computing b_k and perservation of energy. Add some comment

--- fourier/transform.go
+++ fourier/transform.go
@@ -26,21 +26,21 @@ func (c Complex) String() string {
 		if c.Re < EPS && c.Re > 0-EPS {
 			return fmt.Sprintf("0")
 		}
-		return fmt.Sprintf("%.3f", c.Re)
+		return fmt.Sprintf("%f", c.Re)
 	}
 
 	//(a - bi)
 	if c.Im < 0.0 {
 		if c.Re < EPS && c.Re > 0-EPS {
-			return fmt.Sprintf("0 - i(%.3f)", math.Abs(c.Im))
+			return fmt.Sprintf("0 - i(%f)", math.Abs(c.Im))
 		}
-		return fmt.Sprintf("%.3f - i(%.3f)", c.Re, math.Abs(c.Im))
+		return fmt.Sprintf("%f - i(%f)", c.Re, math.Abs(c.Im))
 	}
 	// (a + bi)
 	if c.Re < EPS && c.Re > 0-EPS {
-		return fmt.Sprintf("0 + i(%.3f)", c.Im)
+		return fmt.Sprintf("0 + i(%f)", c.Im)
 	}
-	return fmt.Sprintf("%.3f + i(%.3f)", c.Re, c.Im)
+	return fmt.Sprintf("%f + i(%f)", c.Re, c.Im)
 }
 
 func (c *Complex) swapped() {
@@ -77,13 +77,7 @@ func (c Complex) add(o Complex) Complex {
 	}
 }
 
-//PrintSignal brints the given signal for test purposes in the format a + bi\n...
-func PrintSignal(x []Complex) {
-	for _, xi := range x {
-		fmt.Printf("%s\n", xi)
-	}
-}
-
+// e^(⁻² ᵖᶦ ᶦᵏʲ/ᴺ)
 func e(k, j int, n int) Complex {
 	return Complex{
 		Re: math.Cos(2 * math.Pi * float64(k) * float64(j) / float64(n)),
@@ -113,10 +107,9 @@ func Edot(k, s, n int) Complex {
 	return sum
 }
 
+// a_k = (? 1/N) j=0^N-1 (b_j.e ^(-2πijk/N))
 func a(k int, b []Complex, n int) Complex {
 	sum := zeroComplex()
-	// b[1000] = 0 + i0.5
-
 	for j := 0; j <= n/2-1; j++ {
 		d := dot(b[j], e(k, j, n))
 		sum.added(d)
@@ -125,10 +118,11 @@ func a(k int, b []Complex, n int) Complex {
 		sum.added(d)
 	}
 
-	// sum.divided(float64(len(c)))
-	return sum
+	return sum.divide(float64(n))
 }
 
+// b_k = (? 1/N) * Σ_j=0^N-1 (a_j * e ^(-2πijk/N))
+// follows this formula, but with a_j - real
 func b_real(k int, x []float64) Complex {
 	sum := zeroComplex()
 
@@ -139,10 +133,12 @@ func b_real(k int, x []float64) Complex {
 		sum.added(d)
 	}
 
-	sum.divided(float64(len(x)))
+	// This is for normalisation if you want to keep the energy
+	// sum.divided(float64(len(x)))
 	return sum
 }
 
+// b_k = (1/N) * Σ_j=0^N-1 (a_j * e ^(-2πijk/N))
 func b(k int, x []Complex) Complex {
 	sum := zeroComplex()
 	for j := 0; j < len(x); j++ {
@@ -150,25 +146,32 @@ func b(k int, x []Complex) Complex {
 		sum.added(d)
 	}
 
-	sum.divided(float64(len(x)))
+	// Normalisation: This is for normalisation if you want to keep the energy
+	// sum.divided(float64(len(x)))
 	return sum
 }
 
+// Power returns the power of the given complex number
+// a + ib
+// a^2 + b^2
 func Power(c Complex) float64 {
 	return (c.Re*c.Re + c.Im*c.Im)
 }
 
+// Magnitude returns the magnitude of the given complex number
+// a + ib
+// √(a^2 + b^2)
 func Magnitude(c Complex) float64 {
 	return math.Sqrt(c.Re*c.Re + c.Im*c.Im)
 }
 
+// Idft returns the inverse descrete fourier transform
 func Idft(c []Complex) []Complex {
 	n := (len(c) - 1) * 2
 	x := make([]Complex, n, n)
 
 	for k := 0; k < n; k++ {
 		x[k] = a(k, c, n)
-		x[k].divided(2)
 	}
 
 	return x
@@ -179,8 +182,12 @@ func b_k_fast(x []Complex, W []Complex, depth int, first int, step int, len int)
 		return x[first]
 	}
 
-	return b_k_fast(x, W, depth+1, first, step*2, len/2).add(dot(W[depth], b_k_fast(x, W, depth+1, first+step, step*2, len/2))).divide(2)
+	// The idea is to split the signal on even and odd part so we store the begining of the array and the step
+	// the term represents the even part and the right - the odd part (scaled with e^...)
+	return b_k_fast(x, W, depth+1, first, step*2, len/2).add(dot(W[depth], b_k_fast(x, W, depth+1, first+step, step*2, len/2)))
 
+	// Normalisation: The /2 is for normalisation if you want to keep the energy
+	// return b_k_fast(x, W, depth+1, first, step*2, len/2).add(dot(W[depth], b_k_fast(x, W, depth+1, first+step, step*2, len/2))).divide(2)
 }
 
 func fft(x []Complex, W [][]Complex) []Complex {
@@ -193,6 +200,8 @@ func fft(x []Complex, W [][]Complex) []Complex {
 	return coefficients
 }
 
+// Fft computes the fast fourier transform on the given signal with len N
+// it returns N complex coefficients
 func Fft(x []Complex) []Complex {
 	n := len(x)
 
@@ -208,6 +217,7 @@ func Fft(x []Complex) []Complex {
 	return fft(x, W)
 }
 
+// Ifft returns the inverse fast fourier transform of the given fourier coefficients
 func Ifft(x []Complex) []Complex {
 	n := len(x)
 
@@ -228,16 +238,20 @@ func Ifft(x []Complex) []Complex {
 	coefficients := fft(x, W)
 	for i := 0; i < n; i++ {
 		coefficients[i].swapped()
+		coefficients[i].divided(float64(len(x)))
 	}
 
 	return coefficients
 }
 
+// FftReal returns the fourier coefficients for the given signal of len N
+// it returns N/2 + 1 coefficients and works only with powers of two
 func FftReal(x []float64) []Complex {
 	if !IsPowerOfTwo(len(x)) {
 		panic("FFT expects the len of the data to be a power of 2")
 	}
 
+	// split the signal on even and odd part
 	n := len(x)
 	even := make([]float64, n/2, n/2)
 	odd := make([]float64, n/2, n/2)
@@ -245,22 +259,28 @@ func FftReal(x []float64) []Complex {
 	X := make([]Complex, n/2+1, n/2+1)
 
 	for k := 0; k < n/2; k++ {
+		// calculate the special N/2 coefficient
+		//since it's the sum of the signal with alternating signs
 		X[n/2].Re += x[2*k] - x[2*k+1]
 		even[k] = x[2*k]
 		odd[k] = x[2*k+1]
 	}
+
 	Even, Odd := DoubleReal(even, odd)
 
 	for k := 0; k < n/2; k++ {
 		X[k] = Even[k].add(dot(e(k, 1, n).conjugate(), Odd[k]))
 	}
 
-	X[0].divided(2.0)
-	X[n/2].divided(float64(n))
+	//  Normalisation: This is again for normalisation because the first and last coefficients don't have conjugates, so the energy shouldn't be doubled.
+	// X[0].divided(2.0)
+	// X[n/2].divided(float64(n))
 
 	return X
 }
 
+// FftWav returns the fourier coefficients for the given wav file of len N
+// It returns N/2 + 1 coefficients
 func FftWav(f WavFile) []Complex {
 	if !IsPowerOfTwo(len(f.data)) {
 		panic("FFT expects the len of the data to be a power of 2")
@@ -269,13 +289,15 @@ func FftWav(f WavFile) []Complex {
 	return FftReal(f.data)
 }
 
+// DoubleReal returns the fourier coefficients for the two given real signals of len N
+// it composes a new complex signal - its real part is the first signal and the imaginary part is the second signals
+// It then runs Fft fot the newly composed complex signal and then separates the coefficients which are with len N
 func DoubleReal(x, y []float64) ([]Complex, []Complex) {
 	n := len(x)
 
 	xCoefficients := make([]Complex, n, n)
 	yCoefficients := make([]Complex, n, n)
 
-	//TODO: check this
 	z := make([]Complex, n, n)
 	for i := 0; i < n; i++ {
 		xCoefficients[0].Re += x[i]
@@ -285,8 +307,9 @@ func DoubleReal(x, y []float64) ([]Complex, []Complex) {
 		z[i].Im = y[i]
 	}
 
-	xCoefficients[0].Re = xCoefficients[0].Re / float64(n)
-	yCoefficients[0].Re = yCoefficients[0].Re / float64(n)
+	// Normalisation: Because the first coefficient is calculated as a special case, this is for normalisation
+	// xCoefficients[0].Re = xCoefficients[0].Re / float64(n)
+	// yCoefficients[0].Re = yCoefficients[0].Re / float64(n)
 
 	zCoefficients := Fft(z)
 	for k := 1; k < n; k++ {
@@ -300,7 +323,8 @@ func DoubleReal(x, y []float64) ([]Complex, []Complex) {
 	return xCoefficients, yCoefficients
 }
 
-//Dft returns the discrete fourier transform
+//Dft returns the discrete fourier transform for complex signal with len N
+// it return N coefficients [0...N-1]
 func Dft(x []Complex) []Complex {
 	coefficients := make([]Complex, len(x), len(x))
 	for k := 0; k < len(x); k++ {
@@ -309,15 +333,17 @@ func Dft(x []Complex) []Complex {
 	return coefficients
 }
 
-//Dft returns the discrete fourier transform
+//Dft returns the discrete fourier transform for real signal with len N
+// it returns N/2 + 1 coefficients [0....N/2]
 func DftReal(x []float64) []Complex {
 	coefficients := make([]Complex, len(x)/2+1, len(x)/2+1)
 	for k := 0; k < len(x)/2+1; k++ {
 		coefficients[k] = b_real(k, x)
 
-		if k > 0 && k < len(x)/2 {
-			coefficients[k].divided(0.5)
-		}
+		// Energy: This is for saving energy
+		// if k > 0 && k < len(x)/2 {
+		// 	coefficients[k].divided(0.5)
+		// }
 
 	}
 	return coefficients
