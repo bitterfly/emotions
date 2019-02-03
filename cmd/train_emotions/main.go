21b23885b7fe5e647e5e170ca6369aaa6ec36f90
bitterfly <dageneva@gmail.com>
2019-02-03 21:32:46 +0200

    Make main take max and min k

--- cmd/train_emotions/main.go
+++ cmd/train_emotions/main.go
@@ -37,68 +37,46 @@ func main() {
 		panic("go run main.go <k> <dir-template> <--emotion1 emotion1.wav [emotion1.wav... --emotion2]>")
 	}
 
-	testK := false
 	outputDirIndex := 2
+	maxK := -1
+
 	k, err := strconv.Atoi(os.Args[1])
 	if err != nil {
-		testK = true
-		outputDirIndex = 1
-
+		panic("Must provide k")
+	}
+	if _, err := strconv.Atoi(os.Args[2]); err != nil {
+		outputDirIndex = 2
+		maxK = k
+	} else {
+		maxK, _ = strconv.Atoi(os.Args[2])
+		outputDirIndex = 3
 	}
 
 	outputDir := os.Args[outputDirIndex]
-
 	emotionFiles := emotions.ParseArguments(os.Args[outputDirIndex+1:])
 
-	if testK {
-		for j := 2; j < 12; j++ {
-			if _, err := os.Stat(fmt.Sprintf("%s_k%d", outputDir, j)); os.IsNotExist(err) {
-				os.Mkdir(fmt.Sprintf("%s_k%d", outputDir, j), 0775)
-			}
+	for j := k; j <= maxK; j++ {
+
+		if _, err := os.Stat(fmt.Sprintf("%s_k%d", outputDir, j)); os.IsNotExist(err) {
+			os.Mkdir(fmt.Sprintf("%s_k%d", outputDir, j), 0775)
 		}
+	}
 
-		for emotion, files := range emotionFiles {
-			mfccs := readEmotion(files)
-			for j := 2; j < 12; j++ {
-				fmt.Fprintf(os.Stderr, "%s %d\n", emotion, j)
-				egm := emotions.EmotionGausianMixure{
-					Emotion: emotion,
-					GM:      getGMM(mfccs, j),
-				}
-
-				bytes, err := json.Marshal(egm)
-				if err != nil {
-					panic(fmt.Sprintf("Error when marshaling %s with k %d\n", emotion, j))
-				}
-
-				filename := path.Join(fmt.Sprintf("%s_k%d", outputDir, j), fmt.Sprintf("%s.gmm", emotion))
-				fmt.Fprintf(os.Stderr, filename)
-				ioutil.WriteFile(filename, bytes, 0644)
-			}
-		}
-	} else {
-		outputDir = fmt.Sprintf("%s_k%d", outputDir, k)
-		if _, err := os.Stat(outputDir); os.IsNotExist(err) {
-			os.Mkdir(outputDir, 0775)
-		}
-
-		egms := make([]emotions.EmotionGausianMixure, len(emotionFiles), len(emotionFiles))
-		i := 0
-		for emotion, files := range emotionFiles {
-			egms[i] = emotions.EmotionGausianMixure{
+	for emotion, files := range emotionFiles {
+		mfccs := readEmotion(files)
+		for j := k; j <= maxK; j++ {
+			fmt.Fprintf(os.Stderr, "%s %d\n", emotion, j)
+			egm := emotions.EmotionGausianMixure{
 				Emotion: emotion,
-				GM:      getGMMfromEmotion(files, k),
+				GM:      getGMM(mfccs, j),
 			}
-			i++
-		}
 
-		for i := 0; i < len(egms); i++ {
-			bytes, err := json.Marshal(egms[i])
+			bytes, err := json.Marshal(egm)
 			if err != nil {
-				panic(fmt.Sprintf("Error when marshaling %s\n", egms[i].Emotion))
+				panic(fmt.Sprintf("Error when marshaling %s with k %d\n", emotion, j))
 			}
-			filename := path.Join(outputDir, fmt.Sprintf("%s.gmm", egms[i].Emotion))
-			fmt.Fprintf(os.Stderr, "Writing to %s\n", filename)
+
+			filename := path.Join(fmt.Sprintf("%s_k%d", outputDir, j), fmt.Sprintf("%s.gmm", emotion))
 			fmt.Fprintf(os.Stderr, filename)
 			ioutil.WriteFile(filename, bytes, 0644)
 		}
